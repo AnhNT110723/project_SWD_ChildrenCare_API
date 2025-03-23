d99b66beddcfe339d396d7255c8422264a7f089c
Phạm Cát Tường <130692751+C4tTu0n9@users.noreply.github.com>
2025-03-23 21:52:25 +0700

    feat: Add CRUD Slider for Manager (#9)

---
+++ Children_Care_API/Children_Care_API/DTOs/SliderDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Children_Care_API.DTOs
+{
+    public class SliderDto
+    {
+        public string Title { get; set; }
+        public string Image { get; set; }
+        public string Backlink { get; set; }
+        public bool Status { get; set; }
+    }
+}
